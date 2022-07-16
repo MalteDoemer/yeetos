cef35e272e4b00a4c924dea713a95cc43f05bcd0
Malte Dömer <maltedoemer@gmail.com>
2022-07-16 17:43:49 +0200

    started with boot info

--- crates/bootinfo/src/lib.rs
+++ crates/bootinfo/src/lib.rs
@@ -1,1 +1,15 @@
+#![no_std]
 
+use memory::VirtAddr;
+
+#[repr(C)]
+pub struct BootInfo { 
+    /// Virtual address of the initial ram disk
+    pub initrd_addr: VirtAddr,
+    /// Size of the initial ram disk in bytes
+    pub initrd_size: usize,
+    /// Virtual address of the memory map
+    pub mmap_addr: VirtAddr,
+    /// Number of memory map entries
+    pub mmap_count: usize,
+}

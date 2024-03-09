fb61eb65a869d37db17de90457f41a9d1767b0cb
Malte Dömer <maltedoemer@gmail.com>
2024-03-09 16:22:40 +0100

    KernelImage: added function to compute the kernel image size

--- loaders/multiboot2/src/kernel_image.rs
+++ loaders/multiboot2/src/kernel_image.rs
@@ -1,5 +1,5 @@
-use elf::{endian::NativeEndian, segment::SegmentTable, ElfBytes, ParseError};
-use memory::VirtAddr;
+use elf::{abi::PT_LOAD, endian::NativeEndian, segment::SegmentTable, ElfBytes, ParseError};
+use memory::{VirtAddr, PAGE_SIZE};
 
 /// This struct represents the to be loaded kernel image.
 /// It contains the file data of the kernel image and
@@ -28,5 +28,20 @@ impl<'a> KernelImage<'a> {
 
     pub fn segments(&self) -> Option<SegmentTable<'a, NativeEndian>> {
         self.elf_image.segments()
+    }
+
+    pub fn compute_in_memory_size(&self) -> usize {
+        // TODO: also factor in the stack size
+
+        self.segments()
+            .expect("could not find segment info in kernel executable")
+            .iter()
+            .filter(|seg| seg.p_type == PT_LOAD)
+            .map(|seg| (seg.p_memsz as usize).next_multiple_of(PAGE_SIZE))
+            .sum()
+    }
+
+    pub fn compute_load_end_address(&self) -> VirtAddr {
+        self.load_addr + self.compute_in_memory_size()
     }
 }

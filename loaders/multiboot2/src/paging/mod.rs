7add4072224263a32e9146541a793df17e3587a7
Malte Dömer <maltedoemer@gmail.com>
2024-03-29 11:16:57 +0100

    multiboot2: added PageMapLevelFourGuard

--- loaders/multiboot2/src/paging/mod.rs
+++ loaders/multiboot2/src/paging/mod.rs
@@ -1,26 +1,52 @@
 //! The 64-bit kernel uses 4 Level paging.
 //! The (current) page tables will be accessed using the recursive mapping technique.
 //!
-//! After gaining control from boot.s only the first 4GiB will be identity mapped using 4MiB pages.
-//!
-//! Thus we need to "enable" recursive mapping and also "enable" the higher-half mapping.
+use core::ops::{Deref, DerefMut};
 
 use log::info;
 use memory::paging::{Level4, Table};
-use spin::Mutex;
+use spin::{Mutex, MutexGuard};
 
 /// This is a pointer to the recursive mapped pml4t.
 /// In order to access it the page_lock must be held.
-pub const P4: *mut Table<Level4> = 0xffffffff_fffff000 as *mut _;
+const P4: *mut Table<Level4> = 0xffffffff_fffff000 as *mut _;
 
 /// The `PAGE_LOCK` must be held for any access to the recursive mapping area and other paging operations.
-pub static PAGE_LOCK: Mutex<()> = Mutex::new(());
+static PAGE_LOCK: Mutex<()> = Mutex::new(());
+
+pub struct PageMapLevelFourGuard<'a> {
+    table: &'a mut Table<Level4>,
+    guard: MutexGuard<'a, ()>,
+}
+
+impl<'a> Deref for PageMapLevelFourGuard<'a> {
+    type Target = Table<Level4>;
+
+    fn deref(&self) -> &Self::Target {
+        self.table
+    }
+}
+
+impl<'a> DerefMut for PageMapLevelFourGuard<'a> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.table
+    }
+}
+
+/// Obtain a reference to the recursively mapped pml4t.
+/// This function locks the `PAGE_LOCK` and returns a guard object
+/// that releases the lock when dropped.
+pub fn get_page_map_level_four() -> PageMapLevelFourGuard<'static> {
+    let guard = PAGE_LOCK.lock();
+    let table = unsafe { &mut *P4 };
+
+    PageMapLevelFourGuard { table, guard }
+}
 
 pub fn test() {
-    let _guard = PAGE_LOCK.lock();
-    let pml4t = unsafe { &mut *P4 };
+    let p4 = get_page_map_level_four();
 
-    let pdpt = unsafe { pml4t.next_table(0).unwrap() };
+    let pdpt = unsafe { p4.next_table(0).unwrap() };
     let pdt = unsafe { pdpt.next_table(0).unwrap() };
 
     let entry = &pdt[0];
